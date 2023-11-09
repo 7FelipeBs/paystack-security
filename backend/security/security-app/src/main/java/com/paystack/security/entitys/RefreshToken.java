aa4aac3aad328b812536c7d9df0db74a2aded8dc
Felipe Bs <programming1129@gmail.com>
2023-11-09 17:38:42 -0300

    add refresh token + cookies http

---
+++ backend/security/security-app/src/main/java/com/paystack/security/entitys/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.paystack.security.entitys;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "refresh_token")
+public class RefreshToken {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "refresh_token_id")
+	private long id;
+
+	@OneToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	private Users user;
+
+	@Column(nullable = false, unique = true)
+	private String token;
+
+	@Column(nullable = false)
+	private Instant expiryDate;
+}
+
--- backend/security/security-app/src/main/java/com/paystack/security/services/AuthService.java
+++ backend/security/security-app/src/main/java/com/paystack/security/services/AuthService.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,18 +14,21 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.paystack.security.entitys.RefreshToken;
 import com.paystack.security.entitys.Roles;
 import com.paystack.security.entitys.Users;
 import com.paystack.security.enums.ERole;
+import com.paystack.security.exceptions.TokenRefreshException;
 import com.paystack.security.repositorys.IRolesRepository;
 import com.paystack.security.repositorys.IUsersRepository;
 import com.paystack.security.utils.JwtUtils;
 import com.paystack.security.views.UserDetailsView;
 import com.paystack.security.views.request.LoginRequestView;
 import com.paystack.security.views.request.SignupRequestView;
-import com.paystack.security.views.response.JwtResponseView;
 import com.paystack.security.views.response.MessageResponseView;
+import com.paystack.security.views.response.UserInfoResponseView;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.NonNull;
 
 @Service
@@ -36,35 +41,74 @@ public class AuthService {
 	private final @NonNull IRolesRepository rolesRepository;
 
 	private final @NonNull PasswordEncoder encoder;
+	
+	private final @NonNull RefreshTokenService refreshTokenService;
 
 	private final @NonNull JwtUtils jwtUtils;
-	
-	private static final String MsgError = "Error: Role is not found.";
 
-	public AuthService(AuthenticationManager authenticationManager, JwtUtils jwtUtils,
-			IUsersRepository usersRepository, IRolesRepository rolesRepository, PasswordEncoder encoder) {
-
-		this.jwtUtils = jwtUtils;
+	public AuthService(AuthenticationManager authenticationManager, JwtUtils jwtUtils, IUsersRepository usersRepository,
+			IRolesRepository rolesRepository, PasswordEncoder encoder, RefreshTokenService refreshTokenService) {
 		this.usersRepository = usersRepository;
 		this.rolesRepository = rolesRepository;
-		this.encoder = encoder;
+
+		this.refreshTokenService = refreshTokenService;
+		
 		this.authenticationManager = authenticationManager;
+		this.jwtUtils = jwtUtils;
+		this.encoder = encoder;
 	}
-	
+
 	public ResponseEntity<?> signin(LoginRequestView loginRequest) {
 
 		Authentication authentication = authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = jwtUtils.generateJwtToken(authentication);
 
 		UserDetailsView userDetails = (UserDetailsView) authentication.getPrincipal();
-		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
-				.toList();
 
-		return ResponseEntity.ok(
-				new JwtResponseView(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
+		ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority()).toList();
+
+		RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
+
+		ResponseCookie jwtRefreshCookie = jwtUtils.generateRefreshJwtCookie(refreshToken.getToken());
+
+		return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+				.header(HttpHeaders.SET_COOKIE, jwtRefreshCookie.toString()).body(new UserInfoResponseView(
+						userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
+	}
+
+	public ResponseEntity<?> logoutUser() {
+		Object principle = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (!"anonymousUser".equals(principle.toString())) {
+			Long userId = ((UserDetailsView) principle).getId();
+			refreshTokenService.deleteByUserId(userId);
+		}
+
+		ResponseCookie jwtCookie = jwtUtils.getCleanJwtCookie();
+		ResponseCookie jwtRefreshCookie = jwtUtils.getCleanJwtRefreshCookie();
+
+		return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+				.header(HttpHeaders.SET_COOKIE, jwtRefreshCookie.toString())
+				.body(new MessageResponseView("You've been signed out!"));
+	}
+
+	public ResponseEntity<?> refreshtoken(HttpServletRequest request) {
+		String refreshToken = jwtUtils.getJwtRefreshFromCookies(request);
+
+		if ((refreshToken != null) && (refreshToken.length() > 0)) {
+			return refreshTokenService.findByToken(refreshToken).map(refreshTokenService::verifyExpiration)
+					.map(RefreshToken::getUser).map(user -> {
+						ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(user);
+
+						return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+								.body(new MessageResponseView("Token is refreshed successfully!"));
+					}).orElseThrow(() -> new TokenRefreshException(refreshToken, "Refresh token is not in database!"));
+		}
+
+		return ResponseEntity.badRequest().body(new MessageResponseView("Refresh Token is empty!"));
 	}
 
 	public ResponseEntity<?> signup(SignupRequestView signUpRequest) {
@@ -83,6 +127,7 @@ public class AuthService {
 		Set<String> strRoles = signUpRequest.getRole();
 		Set<Roles> roles = new HashSet<>();
 
+		final String MsgError = "Error: Role is not found.";
 		if (strRoles == null) {
 			Roles userRole = rolesRepository.findByName(ERole.ROLE_USER)
 					.orElseThrow(() -> new RuntimeException(MsgError));
